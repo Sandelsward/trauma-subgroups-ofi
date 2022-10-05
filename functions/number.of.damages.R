1cc2373c71b1fe84e7f78930c4f60c4116567631
George Sandelsward <georges@test.noacs.io>
2022-10-05 17:30:16 +0200

    .

--- functions/number.of.damages.R
+++ functions/number.of.damages.R
@@ -1,1 +1,7 @@
 #Funkton för att avgöra om det finns en eller fler skador. Dvs fler ais_first.
+
+number.of.dammages <- function(cleaned.data) {
+  test.vec <- c()
+  max(number.of.dammages, na.rm = FALSE)
+  
+  #cleaned.data$max.ais <- max(as.numeric(substr(as.character(cleaned.data[1,7:56], 8, 8))))
