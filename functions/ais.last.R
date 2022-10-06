c0dc0073698ff6b3c3f9ebe0a82ad0334a6acaf8
Jonatan Attergrim <jonatana@k9tbt01.ki.se>
2022-10-06 15:37:21 +0200

    Changed last/first

--- functions/ais.last.R
+++ functions/ais.last.R
@@ -1,7 +1,8 @@
 # Funktion som skapar ny kolumn med med sista siffran i alla aiskoder.
+# Ändrade koden till sev (severity) från tid last
 
 ais_last <- function(cleaned.data) {
-  returned.data <- cleaned.data %>% mutate(across(7:56, ~substr(.x, 8, 8), .names = "{col}.last"))
+  returned.data <- cleaned.data %>% mutate(across(7:56, ~substr(.x, 8, 8), .names = "{col}.sev"))
  return(returned.data)
 }
   
