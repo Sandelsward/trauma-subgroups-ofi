b5ae1f17a4aa16a6c9199f887a506beb1e0daafc
George Sandelsward <georges@k9tbt01.ki.se>
2022-11-28 09:45:22 +0100

    test

--- main.R
+++ main.R
@@ -32,15 +32,10 @@ library(formattable)
 noacsr::source_all_functions()
 
 ## Import data
-data <- import_data(test = TRUE)
+data <- import_data()
 
 ## Renaming data sets fo work with merge function
 
-data[['swetrau']] <- data$swetrau_scrambled
-data[['fmp']] <- data$fmp_scrambled
-data[['atgarder']] <- data$atgarder_scrambled
-data[['problem']] <- data$problem_scrambled
-data[['kvalgranskning2014.2017']] <- data$kvalgranskning2014.2017_scrambled
 
 merged.data <- merge_data(data)
 ########
