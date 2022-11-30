a8958505fae8d65c56221a617645643557202e0e
George Sandelsward <georges@k9tbt01.ki.se>
2022-11-30 16:16:37 +0100

    uppdaterad diskussion

---
+++ temporary.bib.R
@@ -0,0 +1,142 @@
+[1] @ARTICLE{Adams2012-mo,
+  title     = "Unique pattern of complications in elderly trauma patients at a
+               Level {I} trauma center",
+  author    = "Adams, Sasha D and Cotton, Bryan A and McGuire, Mary F and
+               Dipasupil, Edmundo and Podbielski, Jeanette M and Zaharia,
+               Adrian and Ware, Drue N and Gill, Brijesh S and Albarado, Rondel
+               and Kozar, Rosemary A and Duke, James R and Adams, Philip R and
+               Dyer, Carmel B and Holcomb, John B",
+  abstract  = "BACKGROUND: Trauma centers are caring for increased proportions
+               of elderly patients. Although age and Injury Severity Score are
+               independently associated with mortality, trauma centers were
+               originally designed to care for seriously injured patients
+               without age-specific guidelines. We hypothesized that elderly
+               patients would have different complication patterns than their
+               younger counterparts. METHODS: The trauma registry of an
+               American College of Surgeons -verified Level I trauma center was
+               queried for all patients older than 14 years admitted between
+               January 2005 and December 2008. Mechanism, mortality, and
+               complications were evaluated after dividing patients into eight
+               age groups. RESULTS: Of the 15,223 patients, 13\% were elderly
+               ($\geq$65), and 86\% were injured via a blunt mechanism.
+               Increasing age correlated with fatality (all Injury Severity
+               Scores), end-organ failure, and thromboembolic complications
+               (deep venous thrombosis and coagulopathy). Analysis revealed a
+               significant breakpoint at 45 years of age for mortality,
+               decubitus ulcer, and renal failure (all p values <0.05).
+               Infectious complications (sepsis, wound infection, and abscess)
+               all peaked between 45 years and 65 years and then declined with
+               increasing age. CONCLUSIONS: We document that elderly trauma
+               patients suffer the same complications as their younger
+               counterparts, albeit at a different rate. More importantly, we
+               identified a ``breakpoint'' of increased risk of complications
+               and mortality at greater than 45 years. Although the mechanisms
+               behind these observations remain unknown, understanding their
+               unique patterns may allow appropriate allocation of resources
+               and focus research efforts on interventions that should improve
+               outcomes.",
+  journal   = "J. Trauma Acute Care Surg.",
+  publisher = "Ovid Technologies (Wolters Kluwer Health)",
+  volume    =  72,
+  number    =  1,
+  pages     = "112--118",
+  month     =  jan,
+  year      =  2012,
+  language  = "en"
+}
+
+
+
+[2] @ARTICLE{S_Punla2022-vw,
+  title     = "Are we there yet?: An analysis of the competencies of {BEED}
+               graduates of {BPSU-DC}",
+  author    = "S. Punla, Candida and {https://orcid.org/ 0000-0002-1094-0018,
+               cspunla@bpsu.edu.ph} and C. Farro, Rosemarie and
+               {https://orcid.org/0000-0002-3571-2716, rcfarro@bpsu.edu.ph} and
+               {Bataan Peninsula State University Dinalupihan, Bataan,
+               Philippines}",
+  abstract  = "The Commission on Higher Education is committed to developing
+               competency-based learning standards that comply with existing
+               international standards which is evident with the release of CMO
+               No. 46 s. 2012 and CMO No. 74 s. 2017. This study aims to
+               investigate if the graduates of the BEEd program of Bataan
+               Peninsula State University Dinalupihan Campus have met the
+               expectations provided in CMO No. 74 series of 2017. This study
+               uses an explanatory-sequential design which is a two-phase
+               approach, where quantitative results of the study, from
+               collected quantitative data using a survey questionnaire in
+               phase 1 were followed up and supported by qualitative data
+               collected in phase 2 to further explain the findings. The
+               results of the study indicate that significant difference is
+               evident in the competency of the respondents based on the
+               expected competencies as stated at CMO No. 74, series of 2017 in
+               terms of in-depth understanding of learners; pedagogical content
+               knowledge; assessment and evaluation; communication, higher
+               order thinking, and technology; attributes of model teacher;
+               personal and professional development; when respondents have
+               grouped accordingly, as provided by the p-values which are
+               statistically lesser than the alpha of .05. In general,
+               graduates confidently believes that they possess the
+               competencies stated and describe in CMO No. 74 s. 2017, their
+               Cooperating Teachers and Coordinators, however, based on their
+               qualitative responses, pointed out that there are still areas of
+               competencies that are needed to be developed among the
+               graduates. The result of the study indicates that the
+               institution should revisit the program implementation of CMO No.
+               46 s. 2012, focusing on competencies stated in CMO No. 74 s.
+               2017. Keywords: Competencies, Guidelines, Policies, Standards",
+  journal   = "International Multidisciplinary Research Journal",
+  publisher = "International Organization of Educators and Researchers Inc.",
+  volume    =  4,
+  number    =  3,
+  pages     = "50--59",
+  month     =  sep,
+  year      =  2022
+}
+
+ [3] @ARTICLE{Davoodabadi2021-yv,
+  title     = "Predicting factors and incidence of preventable trauma induced
+               mortality",
+  author    = "Davoodabadi, Abdoulhosein and Abdorrahim Kashi, Esmail and
+               Mohammadzadeh, Mahdi and Mousavi, Noushin and Shafagh, Shima and
+               Ghafoor, Leila and Sehat, Mojtaba and Ale Mohammad, Shahrzad and
+               Hajian, Abbas",
+  abstract  = "INTRODUCTION: Trauma is one of the most common causes of
+               morbidity and mortality worldwide. Since the definition of
+               preventable death has been described many studies like current
+               one were conducted to evaluate this issue. METHODS: This cohort
+               retrospective study investigated archived medical files of
+               trauma victims from 2017 to 2020 in a referral single-center
+               trauma hospital. Registered demographic data, vital signs,
+               Glasgow coma scale (GCS), timing of trauma and death, executed
+               interventions, type and mechanism of trauma in addition to time
+               errors, clinical mismanagements, and missed injuries were
+               extracted. Injury severity score, revised trauma score, and
+               probability of survival based on TRISS method for each case were
+               calculated. Eventually preventable and non-preventable death
+               were defined and compared. RESULTS: Finally from the all 413
+               trauma deaths 246(54.9 \%) files were enrolled. Dead persons
+               were from 18 to 95 years. Of all 189(76.8 \%) were males.
+               Analysis manifested 135(54.9 \%) of all deaths were potentially
+               preventable and the rest 49.1 \% was non-preventable for
+               expiration(p = 0.001). Data showed that from all variables
+               systolic blood pressure $\geq$80 mmHg, respiratory rate >19 per
+               minute, GCS>8, higher RTS, road traffic accidents and control of
+               external bleeding were contributed to prediction of preventable
+               trauma related mortality. CONCLUSION: This study implied on that
+               frequency of trauma related preventable death was regionally
+               high and associating factors that could influence the number of
+               these mortalities included systolic blood pressure, respiratory
+               rate, GCS, revised trauma score, mechanism of trauma, and
+               external bleeding of trauma patients.",
+  journal   = "Ann. Med. Surg. (Lond.)",
+  publisher = "Elsevier BV",
+  volume    =  68,
+  number    =  102609,
+  pages     = "102609",
+  month     =  aug,
+  year      =  2021,
+  keywords  = "Death; ISS; Preventable; Probability of survival; Trauma",
+  copyright = "http://creativecommons.org/licenses/by-nc-nd/4.0/",
+  language  = "en"
+}
